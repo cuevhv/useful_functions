d4f100b8425c58800f255e77605f0f6cb4b20fc8
cuevhv <cuevhv@yahoo.com>
2021-09-04 21:34:50 +0100

    added 2 useful functions

---
+++ compress_file/from_terminal.sh
@@ -0,0 +1,6 @@
+gs -dNOPAUSE -dBATCH -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/printer -sOutputFile=output.pdf input.pdf
+
+
+#Quality level settings are "/screen" the lowest resolution and lowest file size, but fine for viewing on a screen;
+#"/ebook," a mid-point in resolution and file size;
+#"/printer" and "/prepress," high-quality settings used for printing PDFs.
